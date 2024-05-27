fc7ffe4233c9a4dbac9074b35f7f743093ff3075
JohnProgrammin <jokafor809@gmail.com>
2024-05-27 04:48:09 -0700

    Delete Practice-1 directory

--- Practice-1/js/about.js
+++
@@ -1,4 +0,0 @@
-let fahrenheit = prompt("Enter Degree F");
-let converter = fahrenheit - 32;
-let value = converter / 1.8;
-alert(value+" degrees")
--- Practice-1/js/contact.js
+++
@@ -1,3 +0,0 @@
-let usd = prompt("Enter amount in USD");
-let naira = usd * 1000;
-alert("The amount is " + naira + "naira")
